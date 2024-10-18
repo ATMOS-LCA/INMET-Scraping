9007d87a8f351d987373d5315309875f1a4f9f14
Alan A. F. Lopes <alan.lopes@meta.com.br>
2024-10-18 01:43:32 -0300

    fix: mostra segundos ao fim do timestamp do logger

--- Logger.py
+++ Logger.py
@@ -3,6 +3,6 @@ import inspect
 
 
 class Logger:
-    __date_structure = "%d-%m-%Y %H:%M:%s"
+    __date_structure = "%d-%m-%Y %H:%M:%S"
     def log(self, message: str):
         print(f"[{datetime.now().strftime(self.__date_structure)}] {inspect.stack()[1].function} -> {message}")
