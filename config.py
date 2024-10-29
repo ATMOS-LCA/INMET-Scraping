d312220e1b470685b6cdf7ae3ca00527c7a1627e
Alan Albuquerque F. Lopes <alan.alb.flopes@gmail.com>
2024-10-29 20:58:05 -0300

    fix: busca localização atual do script usando comando "pwd"

--- config.py
+++ config.py
@@ -1,8 +1,10 @@
 from json import load
+import subprocess
 import os
 
 def get_config() -> dict:
-    with open(f'{os.path.dirname(os.path.abspath(__file__))}/config.json', 'r') as f:
+    actualPath = subprocess.run("pwd", stdout=subprocess.PIPE, stderr=subprocess.STDOUT).stdout.decode("utf-8").replace('\n', '')
+    with open(f'{actualPath}/config.json', 'r') as f:
         config = load(f)
     if not os.path.exists(config['output_location']): os.mkdir(config['output_location'])
     return config
