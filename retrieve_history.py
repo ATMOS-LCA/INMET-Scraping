09f40984a1fc67ed58e540201c194c31e39f5b4a
Alan A. F. Lopes <alan.lopes@meta.com.br>
2026-02-20 01:38:23 -0300

    feat: novos campos no importador de dados históricos

--- retrieve_history.py
+++ retrieve_history.py
@@ -9,14 +9,22 @@ CONFIG = get_config()
 HIST_FILES_PATH = './data_old_test'
 DB_CONN = psycopg2.connect("dbname=%s user=%s password=%s host=%s port=%s" % (CONFIG["db_database"], CONFIG["db_user"], CONFIG["db_password"], CONFIG["db_host"], CONFIG["db_port"]))
 INSERT_DADO_INMET = """
-INSERT INTO inmet.dados_estacoes (estacao, data, utc, temperatura, umidade, pto_orvalho, pressao, vento, vento_dir, vento_raj, radiacao, chuva)
-VALUES (%(estacao)s, TO_DATE(%(data)s, 'YYYY-MM-DD'), %(utc)s, %(temperatura)s, %(umidade)s, %(pto_orvalho)s, %(pressao)s, %(vento)s, %(vento_dir)s, %(vento_raj)s, %(radiacao)s, %(chuva)s)
+INSERT INTO inmet.dados_estacoes (estacao, data, utc, temperatura, temperatura_min, temperatura_max, umidade, umidade_min, umidade_max, pto_orvalho, pto_orvalho_min, pto_orvalho_max, pressao, pressao_min, pressao_max, vento, vento_dir, vento_raj, radiacao, chuva)
+VALUES (%(estacao)s, TO_DATE(%(data)s, 'YYYY-MM-DD'), %(utc)s, %(temperatura)s, %(temperatura_min)s, %(temperatura_max)s, %(umidade)s, %(umidade_min)s, %(umidade_max)s, %(pto_orvalho)s, %(pto_orvalho_min)s, %(pto_orvalho_max)s, %(pressao)s, %(pressao_min)s, %(pressao_max)s, %(vento)s, %(vento_dir)s, %(vento_raj)s, %(radiacao)s, %(chuva)s)
 ON CONFLICT (estacao, data, utc) 
 DO UPDATE SET
     temperatura = %(temperatura)s,
+    temperatura_min = %(temperatura_min)s,
+    temperatura_max = %(temperatura_max)s,
     umidade     = %(umidade)s,
+    umidade_min = %(umidade_min)s,
+    umidade_max = %(umidade_max)s,
     pto_orvalho = %(pto_orvalho)s,
+    pto_orvalho_min = %(pto_orvalho_min)s,
+    pto_orvalho_max = %(pto_orvalho_max)s,
     pressao     = %(pressao)s,
+    pressao_min = %(pressao_min)s,
+    pressao_max = %(pressao_max)s,
     vento       = %(vento)s,
     vento_dir   = %(vento_dir)s,
     vento_raj   = %(vento_raj)s,
@@ -63,9 +71,17 @@ def start():
             'data': data[0],
             'utc': str(data[1]).replace(':', '').removesuffix(' UTC'),
             'temperatura': sanitize_scrap_number(data[7]),
+            'temperatura_min': sanitize_scrap_number(data[10]),
+            'temperatura_max': sanitize_scrap_number(data[9]),
             'umidade': sanitize_scrap_number(data[15]),
+            'umidade_min': sanitize_scrap_number(data[14]),
+            'umidade_max': sanitize_scrap_number(data[13]),            
             'pto_orvalho': sanitize_scrap_number(data[8]),
-            'pressao': sanitize_scrap_number(data[4]),
+            'pto_orvalho_min': sanitize_scrap_number(data[12]),
+            'pto_orvalho_max': sanitize_scrap_number(data[11]),
+            'pressao': sanitize_scrap_number(data[3]),
+            'pressao_min': sanitize_scrap_number(data[5]),
+            'pressao_max': sanitize_scrap_number(data[4]),
             'vento': sanitize_scrap_number(data[18]),
             'vento_dir': sanitize_scrap_number(data[16]),
             'vento_raj': sanitize_scrap_number(data[17]),
